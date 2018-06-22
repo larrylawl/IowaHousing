d4cbb16a00ee4637a1e67cff726d7a6ef23d06e8
Larry Law <larrylaw97@gmail.com>
2018-06-22 15:06:46 +0800

    First version of XGBR +: to_csv, XGBR_pred -: get_best_leaf_nodes, get_mae

--- Iowa XGBR.py
+++ Iowa XGBR.py
@@ -14,8 +14,9 @@ best_leaf_node: ?
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_absolute_error
-from sklearn.ensemble import RandomForestRegressor
+#from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import Imputer
+from xgboost import XGBRegressor
 
 def impute(data):
     """
@@ -54,53 +55,25 @@ def OHE(data):
     data_dummies =  pd.get_dummies(data_object, dummy_na = True)
     return data_dummies
 
-def get_best_leaf_nodes(train_X, test_X, train_y, test_y):
-    """
-    Input:
-        train_X/y: training data 
-        test_X/y: testing data
-    Output: int, Best Leaf Node
-    """
-    min_mae = train_y.iloc[0]
-    best_leaf_nodes = 0
-    for elt in range(2,1000,50):
-        mae = 0
-        mae = get_mae(train_X, test_X, train_y, test_y, max_leaf_nodes = elt)
-        if mae < min_mae:
-            min_mae = mae
-            best_leaf_nodes = elt
-    return best_leaf_nodes
-
-def get_mae(train_X, test_X, train_y, test_y, max_leaf_nodes = None):
-    """
-    Input: 
-        train_X/y: training data
-        test_X/y: testing data 
-        max_leaf_nodes: int, optional for RandomTreeRegressor. Consider get_best_leaf_nodes
-    Output: MAE
-    """
-    model = RandomForestRegressor(max_leaf_nodes = max_leaf_nodes, random_state=0)
-    model.fit(train_X, train_y)
-    pred_y = model.predict(test_X)
-    mae = mean_absolute_error(test_y, pred_y)
-    return mae
-
-def test_csv(true_test, train_X, test_X, train_y, file_name, max_leaf_nodes = None):
+def to_csv(true_test, pred_y, file_name):
     """
     Input:
         true_test: Original test data. To obtain test ID
-        X: Use entire of training data (not the train_test_split data)
-        y: Testing data
-        file_name: string, name of submission
-        leaf_node: int, optional. Consider get_best_leaf_node function
-    Output: Generates predicted results of test in csv format
+        pred_y: Predicted DV based on XGBR model
+    Output: Converts pred_y to csv format
     """
-    model = RandomForestRegressor(max_leaf_nodes = max_leaf_nodes, random_state=0)
-    model.fit(train_X, train_y) #insert all data for better prediction values
-    test_y = model.predict(test_X)
-    test_df = pd.DataFrame({'Id': true_test.Id, 'SalePrice': test_y})
-    return test_df.to_csv(file_name, index = False)
+    y_df = pd.DataFrame({'Id': true_test.Id, 'SalePrice': test_y})
+    return y_df.to_csv(file_name, index = False)
 
+def XGBR_pred(train_X, test_X, train_y):
+    """
+    Output: Predicted DV based on XGBR model
+    """
+    XGBR_model = XGBRegressor(learning_rate=0.05,n_estimators=1000)
+    XGBR_model.fit(train_X, train_y)
+    pred_y = XGBR_model.predict(test_X)
+    return pred_y
+    
 #Reading Data
 iowa_data = pd.read_csv("Iowa Housing Prices.csv") 
 
@@ -113,13 +86,10 @@ y = iowa_data.SalePrice
 #train_test_split
 from sklearn.model_selection import train_test_split
 train_X, test_X, train_y, test_y = train_test_split(X, y,random_state = 0)
-
-#Align training and test data
 train_X, test_X = train_X.align(test_X, join="inner", axis=1)
 
-#testing get_mae function
-mae = get_mae(train_X, test_X, train_y, test_y)
-best_leaf_nodes = get_best_leaf_nodes(train_X, test_X, train_y, test_y)
+pred_y = XGBR_pred(train_X, test_X, train_y)
+mae = mean_absolute_error(test_y, pred_y)
 
 #Applying on Test data
 #true_test = pd.read_csv("test.csv")
@@ -169,4 +139,19 @@ iowa_submission.to_csv('iowa_submission.csv', index = False)
 
 #Get min mae and best leaf node
 min_mae, best_leaf_node = get_min_mae_and_best_leaf_node(train_X, val_X, train_y, val_y)
+
+def get_best_leaf_nodes(train_X, test_X, train_y, test_y):
+    Input:
+        train_X/y: training data 
+        test_X/y: testing data
+    Output: int, Best Leaf Node
+    min_mae = train_y.iloc[0]
+    best_leaf_nodes = 0
+    for elt in range(2,1000,50):
+        mae = 0
+        mae = get_mae(train_X, test_X, train_y, test_y, max_leaf_nodes = elt)
+        if mae < min_mae:
+            min_mae = mae
+            best_leaf_nodes = elt
+    return best_leaf_nodes
 """
